cc7dfe99723af078876dff0b969d31448a5e5f69
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2021-09-09 11:13:22 -0300

    Implementado TratamentoDAO
    Signed-off-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>

--- src/main/java/com/projeto/projetoveterinaria/model/DAO/TratamentoDAO.java
+++ src/main/java/com/projeto/projetoveterinaria/model/DAO/TratamentoDAO.java
@@ -2,21 +2,101 @@ package com.projeto.projetoveterinaria.model.DAO;
 
 import com.projeto.projetoveterinaria.model.Tratamento;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.List;
 
 public class TratamentoDAO extends DAO<Tratamento> {
     @Override
     protected Tratamento buildObject(ResultSet rs) throws SQLException {
+        Calendar dataEntrada = Calendar.getInstance();
+        dataEntrada.setTime(rs.getDate("dataEntrada"));
+
+        Calendar dataSaida = Calendar.getInstance();
+        dataSaida.setTime(rs.getDate("dataSaida"));
         return new Tratamento(
                 rs.getInt("id"),
                 rs.getString("nome"),
-                rs.getDate("dataEntrada"),
-                rs.getDate("dataSaida"),
+                dataEntrada,
+                dataSaida,
                 rs.getInt("idAnimal"),
                 rs.getBoolean("terminou")
         );
     }
+
+    public Tratamento create(String nome, Calendar dataEntrada, Calendar dataSaida, int idAnimal, boolean terminou) {
+        try {
+            PreparedStatement stmt = DAO.getConnection().prepareStatement("INSERT INTO tratamento (nome, dataIni, dataFim, id_animal, terminado) VALUES  (?,?,?,?,?)");
+            stmt.setString(1, nome);
+            stmt.setDate(2, new Date(dataEntrada.getTimeInMillis()));
+            stmt.setDate(3, new Date(dataSaida.getTimeInMillis()));
+            stmt.setInt(4, idAnimal);
+            stmt.setBoolean(5, terminou);
+            executeUpdate(stmt);
+        } catch (SQLException ex) {
+            System.err.println("EXCEPTION: " + ex.getMessage());
+        }
+        return retrieveLast();
+    }
+
+    public Tratamento retrieveLast() {
+        //language=SQL
+        String query = "SELECT * FROM tratamento WHERE id = (SELECT max(id) FROM tratamento)";
+        List<Tratamento> tratamento = retrieve(query);
+        return tratamento.get(0);
+    }
+
+    public List<Tratamento> retrieveAll() {
+        //language=SQL
+        String quary = "SELECT * FROM tratamento";
+        return retrieve(quary);
+    }
+
+    public Tratamento retrieveById(int id) {
+        //language=SQL
+        String quary = "SELECT * FROM tratamento WHERE id = " + id;
+        List<Tratamento> tratamento = retrieve(quary);
+        return tratamento.get(0);
+    }
+
+    public List<Tratamento> retrieveByIdAnimal(int idAnimal) {
+        //language=SQL
+        String quary = "SELECT * FROM tratamento WHERE id_animal = " + idAnimal;
+        return retrieve(quary);
+    }
+
+    public List<Tratamento> retrieveBySimilarName(String nome) {
+        //language=SQL
+        String query = "SELECT * FROM tratamento WHERE nome LIKE '%" + nome + "%'";
+        return retrieve(query);
+    }
+
+    public void update(Tratamento tratamento) {
+        try {
+            PreparedStatement stmt = DAO.getConnection().prepareStatement("UPDATE tratamento SET nome=?, dataIni=?, dataFim=?,id_animal=?, terminado=? WHERE id=?");
+            stmt.setString(1, tratamento.getNome());
+            stmt.setDate(2, new Date(tratamento.getDataEntrada().getTimeInMillis()));
+            stmt.setDate(3, new Date(tratamento.getDataSaida().getTimeInMillis()));
+            stmt.setInt(4, tratamento.getIdAnimal());
+            stmt.setBoolean(5, tratamento.isTerminou());
+            stmt.setInt(6, tratamento.getId());
+            executeUpdate(stmt);
+        } catch (SQLException ex) {
+            System.err.println("EXCEPTION: " + ex.getMessage());
+        }
+    }
+
+    public void delete(Tratamento tratamento) {
+        try {
+            PreparedStatement stmt = DAO.getConnection().prepareStatement("DELETE FROM  tratamento WHERE id = ?");
+            stmt.setInt(1, tratamento.getId());
+            executeUpdate(stmt);
+        } catch (SQLException ex) {
+            System.err.println("EXCEPTION: " + ex.getMessage());
+        }
+    }
+
 }
